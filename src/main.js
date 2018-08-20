e90bd13c3480b78a19ff1513d33da9e258368a3a
Louay Hamada <loauy.hamada@gmail.com>
2018-08-20 05:02:16 +0300

    Install "Vuex" Plugin

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './app.vue'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
 new Vue({
+  store,
   router,
   render: h => h(App)
 }).$mount('#app')
