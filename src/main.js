2b7e32512f7c7041fccc608662f45bbaf7d31e9d
Louay Hamada <loauy.hamada@gmail.com>
2022-11-28 15:05:55 +0300

    Fix typo in main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuex from 'vuex'
-import App from './app.vue'
+import App from './App.vue'
 import router from './router'
 import store from './store'
 
